b344dc12056249e7b88b2d5b85a90b6a283bae22
lobbychmd <lobbychmd@gmail.com>
2014-01-10 18:43:11 +0800

    3
    3

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using dbshub.Models;
 
 namespace dbshub.Controllers
 {
@@ -11,8 +12,33 @@ namespace dbshub.Controllers
     {
         public ActionResult Index()
         {
+            return RedirectToAction("Tables");
+        }
+
+        public ActionResult Tables()
+        {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Tables( TableSchema table)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(table);
+            }
+            else {
+                var db = l.core.MongoDBHelper.GetMongoDB();
+                var acc = SiteAccount.Get(HttpContext, db);
+                table.Account = acc._id;
+                db.GetCollection("TableSchema").Save(table);
+                return Redirect("/home/tables");
+            }
+        }
+
+        public ActionResult Table()
+        {
+            return View();
+        }
     }
 }
--- Models/TableSchema.cs
+++ Models/TableSchema.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MongoDB.Bson;
 
 namespace dbshub.Models
 {
     public class TableSchema
     {
+        public ObjectId _id { get; set; }
+        public ObjectId Account { get; set; }
         public FieldSchema[] Fields { get; set; }
     }
 
